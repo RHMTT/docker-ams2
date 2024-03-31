3271e5f085d31aff2d99542981d73786f4c539a0
RHMTT <34529464+RHMTT@users.noreply.github.com>
2024-03-31 17:47:05 -0400

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,50 +1,34 @@
 #!/bin/bash
 echo "---Checking if 'accServer.exe' is present---"
-if [ ! -f ${DATA_DIR}/accServer.exe ]; then
-	touch ${DATA_DIR}/place-accServer.exe-here
+if [ ! -f ${DATA_DIR}/ams2-server-manager.exe ]; then
+	touch ${DATA_DIR}/place-ams2-server-manager.exe-here
 	echo "+--------------------------------------------------------"
-	echo "| Assetto Corsa Competizione Server executable not found!"
+	echo "| AMS2 Server executable not found!"
 	echo "| Please be sure to place it in the root folder from the"
 	echo "| Docker container!"
 	echo "|"
-	echo "| You have to download the Assetto Corsa Competizione"
+	echo "| You have to download the AMS2"
 	echo "| Dedicated Server from the Tools section in Steam."
-	echo "| After the download finished right click Assetto Corsa"
-	echo "| Competizione Dedicated Server -> Manage -> Browse local"
-	echo "| files and copy over the 'accServer.exe' from the"
-	echo "| directory .../server/accServer.exe to the root directory"
+	echo "| After the download finished right click AMS2 Server"
+	echo "| -> Manage -> Browse local"
+	echo "| files and copy over the 'ams2-server-manager.exe' from the"
+	echo "| directory .../server/ams2-server-manager.exe to the root directory"
 	echo "| from this Docker container and restart the container."
 	echo "+--------------------------------------------------------"
      
 	chmod -R 777 ${DATA_DIR}
 	sleep infinity
 else
-	if [ -f ${DATA_DIR}/place-accServer.exe-here ]; then
-		rm ${DATA_DIR}/place-accServer.exe-here
+	if [ -f ${DATA_DIR}/place-ams2-server-manager.exe-here ]; then
+		rm ${DATA_DIR}/place-ams2-server-manager.exe-here
 	fi
-	echo "---'accServer.exe' found, continuing...---"
+	echo "---'ams2-server-manager.exe' found, continuing...---"
 fi
 
 echo "---Prepare Server---"
 echo "---Checking if config files are present---"
 if [ ! -d ${DATA_DIR}/cfg ]; then
 	mkdir ${DATA_DIR}/cfg
-fi
-if [ ! -f ${DATA_DIR}/cfg/settings.json ]; then
-	echo "---'settings.json' not found, copying!---"
-	cp /tmp/settings.json ${DATA_DIR}/cfg/
-fi
-if [ ! -f ${DATA_DIR}/cfg/configuration.json ]; then
-	echo "---'configuration.json' not found, copying!---"
-	cp /tmp/configuration.json ${DATA_DIR}/cfg/
-fi
-if [ ! -f ${DATA_DIR}/cfg/event.json ]; then
-	echo "---'event.json' not found, copying!---"
-	cp /tmp/event.json ${DATA_DIR}/cfg/
-fi
-if [ ! -f ${DATA_DIR}/cfg/eventRules.json ]; then
-	echo "---'eventRules.json' not found, copying!---"
-	cp /tmp/eventRules.json ${DATA_DIR}/cfg/
 fi
 
 export WINEARCH=win64
@@ -71,4 +55,4 @@ echo "---Server ready---"
 
 echo "---Start Server---"
 cd ${DATA_DIR}
-wine64 ${DATA_DIR}/accServer.exe
+wine64 ${DATA_DIR}/ams2-server-manager.exe
